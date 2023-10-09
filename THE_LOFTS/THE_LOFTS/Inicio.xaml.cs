c762c0ebdeb58d75786d146c136765cca22d4711
Jorge <jorgepm914@gmail.com>
2023-10-09 00:07:34 -0600

    Agregar archivos de proyecto.

---
+++ THE_LOFTS/THE_LOFTS/Inicio.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace THE_LOFTS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+
+        public async void irPerfil(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Perfil());
+        }
+    }
+}
---
+++ THE_LOFTS/THE_LOFTS/Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace THE_LOFTS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            //btnIngresar.Clicked += (sender,e)=> {
+            //    Navigation.PushAsync(new Inicio());
+            //};
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        public async void btnIngresar(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "Jorge" && txtPass.Text == "jPM230601")
+            {
+                // Navegar a la página principal (MainPage).
+                await Navigation.PushAsync(new Inicio());
+                System.Diagnostics.Debug.WriteLine("Navegación a MainPage exitosa.");
+            }
+            else
+            {
+                // Muestra un mensaje de error en caso de credenciales incorrectas.
+                await DisplayAlert("Error", "Credenciales incorrectas. Intente nuevamente.", "Aceptar");
+                System.Diagnostics.Debug.WriteLine("Navigation es null.");
+            }
+
+        }
+    }
+}
